71ff75c261213ece1702f0abc6613bcacd532643
Eneko <eneko.izaguirre@outlook.com>
2021-05-25 12:19:51 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Example</title>
+    </head>
+    <body>
+
+    <?
+echo 'Hello World'
+?>
+    </body>
+</html>
+
+
